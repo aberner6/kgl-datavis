e7e8ef044e82fd35ef9ed4439793932f25ed48e0
Annelie <annelieberner@gmail.com>
2022-03-08 17:44:04 +0100

    transitions, changing data and images

--- w1/march-08/index.js
+++ w1/march-08/index.js
@@ -1,12 +1,84 @@
-var w = 500;
-var h = 500;
-var rad = 20;
+var slowSpeed = 5;
+var fastSpeed = 28;
+var walker = 100;
+var cyclist = 200;
 
 var svg = d3.select("svg")
-			.attr("width",w)
-			.attr("height",h);
+	.attr("width",1000)
+	.attr("height",800)
 
-var circles = d3.selectAll("circle")
-				.attr("r", rad)
-				.attr("cx", w/2)
-				.attr("cy", h/2);
+
+
+svg.append("line")
+	.attr("x1", walker)
+	.attr("x2", walker+fastSpeed)
+	.attr("y1", cyclist)
+	.attr("y2", cyclist)
+	.attr("stroke", "black")
+for(var i = 0; i<4; i++){
+	svg.append("line")
+		.attr("x1", walker)
+		.attr("x2", walker+slowSpeed)
+		.attr("y1", walker*i)
+		.attr("y2", walker*i)
+		.attr("stroke", "black")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var w = 500;
+// var h = 500;
+// var rad = 20;
+
+// var svg = d3.select("svg")
+// 			.attr("width",w)
+// 			.attr("height",h);
+
+// var circles = d3.selectAll("circle")
+// 				.attr("r", rad)
+// 				.attr("cx", w/2)
+// 				.attr("cy", h/2);
---
+++ w2/march-17/index.js
@@ -0,0 +1,154 @@
+var w = 1000;
+var h = 500;
+var rad = 20;
+var leftMargin = rad*2;
+var imgW = rad*4;
+var imgH = rad*4;
+var svg = d3.select("svg")
+			.attr("width",w)
+			.attr("height",h)
+			.style("background-color","black")
+
+// var arrData = [1, 10, 17, 28];
+// var min = d3.min(arrData);
+// var max = d3.max(arrData);
+
+// var xScale = d3.scaleLinear()
+// 				.domain([min, max])
+// 				.range([leftMargin, w-leftMargin]);
+
+// var circs = svg
+// 	.selectAll('circle')
+// 	.data(arrData)
+// 	.join('circle')
+// 	.attr('cx', w/2)
+// 	.attr('cy', h/2)
+// 	.attr('r', 0)
+// 	.attr('fill','white')
+// 	.transition()
+// 	.attr('r',100)
+
+
+
+
+
+
+
+
+
+// var data = [];
+
+// svg.on("click", function(){
+// 	updateData();
+// 	updateVis();
+// })
+
+// function updateData() {
+//   data = []; //empty the data
+//   for(var i=0; i<5; i++) {
+//     data.push(Math.random()*w);
+//   }
+// }
+// function updateVis() {
+//   d3.select('svg')
+//     .selectAll('circle')
+//     .data(data)
+//     .join('circle')
+//     .attr('cy', h/2)
+//     .attr('r', rad)
+//     .attr('fill','white')
+//     .transition()
+//     .duration(4000)
+//     .attr('cx', function(d) {
+//       return d;
+//     });
+// }
+
+
+
+
+
+// var wholeData = [0,3,5,10,13,15,20,23,25];
+// var partData = [];
+// var index = 0;
+
+// svg.on("click", function(){
+// 	updateData();
+// })
+// var index = 0;
+// var inc = 3;
+// function updateData() {
+// 	partData = []; //empty the data
+// 	for(var i=index; i<index+inc; i++) {
+// 		partData.push(wholeData[i]);
+// 	}
+// 	console.log(partData)
+// 	index = index+inc;
+// 	updateVis();
+// }
+// function updateVis() {
+  // d3.select('svg')
+  //   .selectAll('circle')
+  //   .data(partData)
+  //   .join('circle')
+  //   .attr('cy', h/2)
+  //   .attr('r', rad)
+  //   .attr('fill','white')
+  //   .transition()
+  //   .duration(4000)
+  //   .attr('cx', function(d) {
+  //     return d*10;
+  //   });
+// }
+
+
+
+
+// var imgData = [
+// 	{
+// 	"day":4,
+// 	"img":"img1.jpeg"
+// 	},
+// 	{
+// 	"day":14,
+// 	"img":"img2.jpeg"
+// 	},
+// 	{
+// 	"day":24,
+// 	"img":"img3.jpeg"
+// 	}
+// ];
+
+// var minDay = d3.min(imgData, function(d){
+// 	return d.day;
+// })
+// var maxDay = d3.max(imgData, function(d){
+// 	return d.day;
+// })
+// var dayScale = d3.scaleLinear()
+// 	.domain([minDay, maxDay])
+// 	.range([imgW, w-imgW*2])
+// d3.select('svg')
+//     .selectAll('image')
+//     .data(imgData)
+//     .join('image')
+//     .attr('x', function(d){
+//     	return dayScale(d.day);
+//     })
+//     .attr('y', h/2)
+//     .attr('width', imgW)
+//     .attr('height', imgH)
+// 	.attr("xlink:href", function(d){
+// 		return d.img;
+// 	})
+// 	.transition()
+// 	.duration(3000)
+// 	.attr("y", 0)
+// 	.transition()
+// 	.duration(3000)
+// 	.attr("x",w/2)
+// 	.attr("y",h/2)
+// 	.attr("opacity",.5)
+
+
+
